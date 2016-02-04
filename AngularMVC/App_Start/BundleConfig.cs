5293cf6524a9717a343c6abcd08a1fc9495af924
kylemorton5770 <michael.morton@smail.astate.edu>
2016-02-04 11:17:40 -0600

    init commit

---
+++ AngularMVC/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AngularMVC
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                                  "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
